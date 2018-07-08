28c19fa816849028ccb4af84d584750c7bb7ba25
Dmytriy Myk <porosya@gmail.com>
2018-07-09 00:27:58 +0300

    part3_done

---
+++ Part3.js
@@ -0,0 +1,4 @@
+"use strict";
+var weight = prompt("please type weight in pounds : ");
+alert("Done " + weight + "lbs, it is " + weight * 0.454 + "kilos");
+console.log("conversion done");
---
+++ js/myscript.js
@@ -0,0 +1,4 @@
+'use strict';
+var deposit = prompt('How much would you like to deposit')
+alert("You've deposited: " + deposit)
+console.log('You are cool person')
